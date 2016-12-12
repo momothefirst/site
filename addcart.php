d102db679acd9c8955cc756f32daaeab2141f6fc
momothefirst <ricardo.barrote@gmail.com>
2016-12-12 19:42:22 +0000

    Revert "cart tots working"
    This reverts commit 25315a079bbd4b585b63853143c51445ce59588d.

--- addcart.php
+++ addcart.php
@@ -1,17 +1,7 @@
 <?php
     session_start();
     if (isset($_GET['id'])) {
-        if (count($_SESSION['cart']) > 0) {
-            foreach($_SESSION['cart'] as $value) {
-                if ($value == $_GET['id']) {
-                    break;
-                } else {
-                    array_push($_SESSION['cart'], $_GET['id']);
-                }
-            }
-        } else {
-            array_push($_SESSION['cart'], $_GET['id']);
-        }
+        array_push($_SESSION['cart'], $_GET['id']);
     }
     header("location: product_page.php?id=".$_GET['id']);
 
