25315a079bbd4b585b63853143c51445ce59588d
momothefirst <ricardo.barrote@gmail.com>
2016-12-12 16:55:17 +0000

    cart tots working

--- addcart.php
+++ addcart.php
@@ -1,7 +1,17 @@
 <?php
     session_start();
     if (isset($_GET['id'])) {
-        array_push($_SESSION['cart'], $_GET['id']);
+        if (count($_SESSION['cart']) > 0) {
+            foreach($_SESSION['cart'] as $value) {
+                if ($value == $_GET['id']) {
+                    break;
+                } else {
+                    array_push($_SESSION['cart'], $_GET['id']);
+                }
+            }
+        } else {
+            array_push($_SESSION['cart'], $_GET['id']);
+        }
     }
     header("location: product_page.php?id=".$_GET['id']);
 
