93bbff590b51114d7d354376d2f4b9ac20283295
momothefirst <ricardo.barrote@gmail.com>
2016-12-12 21:26:35 +0000

    tentativa quantidades SESSION ARRAY

--- addcart.php
+++ addcart.php
@@ -1,16 +1,20 @@
 <?php
     session_start();
-    if (isset($_GET['id'])) {
+    if (isset($_GET['id'] && $_GET['qtd'])) {
+        $id = $_GET['id'];
+        $qtd = $_GET['qtd'];
         if (count($_SESSION['cart']) > 0) {
-            foreach($_SESSION['cart'] as $value) {
-                if ($value == $_GET['id']) {
+            foreach($_SESSION['cart'] as $item) {
+                if ($item[$id] == $_GET['id']) {
                     break;
                 } else {
-                    array_push($_SESSION['cart'], $_GET['id']);
+                    $_SESSION['cart'][$id] = $_GET['id'];
+                    $_SESSION['cart'][$qtd] = $_GET['qtd'];
                 }
             }
         } else {
-            array_push($_SESSION['cart'], $_GET['id']);
+            $_SESSION['cart'][$id] = $_GET['id'];
+            $_SESSION['cart'][$qtd] = $_GET['qtd'];
         }
     }
     header("location: product_page.php?id=".$_GET['id']);
