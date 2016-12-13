1fa68bedbaf18ea85bed0eb0f66897b3637795b8
momothefirst <ricardo.barrote@gmail.com>
2016-12-13 15:30:07 +0000

    Revert "tentativa quantidades SESSION ARRAY"
    This reverts commit 93bbff590b51114d7d354376d2f4b9ac20283295.

--- addcart.php
+++ addcart.php
@@ -1,20 +1,16 @@
 <?php
     session_start();
-    if (isset($_GET['id'] && $_GET['qtd'])) {
-        $id = $_GET['id'];
-        $qtd = $_GET['qtd'];
+    if (isset($_GET['id'])) {
         if (count($_SESSION['cart']) > 0) {
-            foreach($_SESSION['cart'] as $item) {
-                if ($item[$id] == $_GET['id']) {
+            foreach($_SESSION['cart'] as $value) {
+                if ($value == $_GET['id']) {
                     break;
                 } else {
-                    $_SESSION['cart'][$id] = $_GET['id'];
-                    $_SESSION['cart'][$qtd] = $_GET['qtd'];
+                    array_push($_SESSION['cart'], $_GET['id']);
                 }
             }
         } else {
-            $_SESSION['cart'][$id] = $_GET['id'];
-            $_SESSION['cart'][$qtd] = $_GET['qtd'];
+            array_push($_SESSION['cart'], $_GET['id']);
         }
     }
     header("location: product_page.php?id=".$_GET['id']);
